f25386f753099b06a1caf9491b782cfdf9c6f3ce
fruitbox12 <dylanwong007@gmail.com>
2023-08-24 06:43:59 -0700

    Update ports.go

--- libdream/common/ports.go
+++ libdream/common/ports.go
@@ -1,40 +1,39 @@
 package common
 
-import (
-	"os"
-	"github.com/joho/godotenv"
-)
+// import (
+//	"os"
+//	"github.com/joho/godotenv"
+// )
 
-func init() {
+//func init() {
 	// Load .env file
-	if err := godotenv.Load(); err != nil {
-		panic("Error loading .env file")
-	}
+//	if err := godotenv.Load(); err != nil {
+//		panic("Error loading .env file")
+//	}
 
-	DefaultAuthPort = getEnvInt("DEFAULT_AUTH_PORT", 121)
-	DefaultHoarderPort = getEnvInt("DEFAULT_HOARDER_PORT", 142)
-	// ... continue with other variables
-	DefaultHost = getEnv("DEFAULT_HOST", "127.0.0.1")
-	DefaultP2PListenFormat = "/ip4/" + DefaultHost + "/tcp/%d"
-	DefaultHTTPListenFormat = "%s://" + DefaultHost + ":%d"
-}
+//	DefaultAuthPort = getEnvInt("DEFAULT_AUTH_PORT", 121)
+//	DefaultHoarderPort = getEnvInt("DEFAULT_HOARDER_PORT", 142)
+//	DefaultHost = getEnv("DEFAULT_HOST", "127.0.0.1")
+//	DefaultP2PListenFormat = "/ip4/" + DefaultHost + "/tcp/%d"
+//	DefaultHTTPListenFormat = "%s://" + DefaultHost + ":%d"
+//}
 
-func getEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-		return value
-	}
-	return fallback
-}
+// func getEnv(key, fallback string) string {
+//	if value, ok := os.LookupEnv(key); ok {
+//		return value
+//	}
+//	return fallback
+ // }
 
-func getEnvInt(key string, fallback int) int {
-	if value, ok := os.LookupEnv(key); ok {
+// func getEnvInt(key string, fallback int) int {
+//	if value, ok := os.LookupEnv(key); ok {
 		// Convert the string to an integer
-		if intValue, err := strconv.Atoi(value); err == nil {
-			return intValue
-		}
-	}
-	return fallback
-}
+//		if intValue, err := strconv.Atoi(value); err == nil {
+//			return intValue
+//		}
+//	}
+//	return fallback
+// }
 
 var (
 	DefaultAuthPort      = 121
