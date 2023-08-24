6322f939b1b1334473525a33afbe2e3524b9ac49
fruitbox12 <dylanwong007@gmail.com>
2023-08-24 06:42:30 -0700

    Update ports.go

--- libdream/common/ports.go
+++ libdream/common/ports.go
@@ -1,5 +1,41 @@
 package common
 
+import (
+	"os"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	// Load .env file
+	if err := godotenv.Load(); err != nil {
+		panic("Error loading .env file")
+	}
+
+	DefaultAuthPort = getEnvInt("DEFAULT_AUTH_PORT", 121)
+	DefaultHoarderPort = getEnvInt("DEFAULT_HOARDER_PORT", 142)
+	// ... continue with other variables
+	DefaultHost = getEnv("DEFAULT_HOST", "127.0.0.1")
+	DefaultP2PListenFormat = "/ip4/" + DefaultHost + "/tcp/%d"
+	DefaultHTTPListenFormat = "%s://" + DefaultHost + ":%d"
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+func getEnvInt(key string, fallback int) int {
+	if value, ok := os.LookupEnv(key); ok {
+		// Convert the string to an integer
+		if intValue, err := strconv.Atoi(value); err == nil {
+			return intValue
+		}
+	}
+	return fallback
+}
+
 var (
 	DefaultAuthPort      = 121
 	DefaultHoarderPort   = 142
